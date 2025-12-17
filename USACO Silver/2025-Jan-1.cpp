4fbee912fce8635213a2324d5d7e3a48b6e22446
David Xiao <davidx1322@gmail.com>
2025-12-17 13:48:43 -0800

    update solution

--- USACO Silver/2025-Jan-1.cpp
+++ USACO Silver/2025-Jan-1.cpp
@@ -1,103 +1,86 @@
-#define ll long long
-#define pb push_back 
-#define v vector
-#define fileread(file) ifstream fin; fin.open((string)file + ".in"); ofstream fout; fout.open((string)file + ".out")
-#define MP(x, y) make_pair(x, y)
-#define F first
-#define S second
-#define For(i, end) for (ll i = 0; i < end; i++)
-#define f0r(i, begin, end) for (ll i = begin; i < end; i++) 
-
 #include <bits/stdc++.h>
 using namespace std;
 
-ostream& operator<<(ostream& os, pair<ll, ll>& p) {
-    return os << p.F << " " << p.S;
-}
-ostream& operator<<(ostream& os, v<ll>& arr) {
-    for (ll ___a : arr) {
-        os << ___a << " ";
-    }
-    return os;
-}
-
 int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    ll num_cows; 
-    cin >> num_cows;
+	int num_cows;
+	cin >> num_cows;
 
-    v<ll> position;
-    
-    ll _p;
-    For (i, num_cows) {
-        cin >> _p; 
-        position.pb(_p - 1);
-    }
+	vector<int> initial_order(num_cows);
 
+	for (int i = 0; i < num_cows; i++) { cin >> initial_order[i]; }
 
-    v<ll> ideal;
+	vector<int> ideal_order(num_cows);
 
-    For (i, num_cows) {
-        cin >> _p;
-        ideal.pb(_p - 1);
-    }
+	for (int i = 0; i < num_cows; i++) { cin >> ideal_order[i]; }
 
-    unordered_map<ll, v<ll>> edge;
+	// Map a cow's species to an array of minimum distances to the edge for that species
+	// in the ideal ordering
+	unordered_map<int, vector<int>> edge_arrays;
 
-    For(i, num_cows) {
-        
-        if (edge.count(ideal[i]) == 0) {
-            v<ll> new_edge = {min(i + 1, num_cows - i)};
-            edge.insert(MP(ideal[i], new_edge));
-        }
+	for (int i = 0; i < num_cows; i++) {
+		int min_distance_to_edge = min(i + 1, num_cows - i);
 
-        else {
-            edge.at(ideal[i]).pb(min(i + 1, num_cows - i));
-        }
-    }
+		// If an array doesn't already exist for a cow species, create a new array for
+		// that species
+		if (edge_arrays.count(ideal_order[i]) == 0) {
+			vector<int> new_edge_array = {min_distance_to_edge};
+			edge_arrays.insert(make_pair(ideal_order[i], new_edge_array));
+		}
 
-    unordered_map<ll, v<ll>> prefix;
+		else {
+			edge_arrays.at(ideal_order[i]).push_back(min_distance_to_edge);
+		}
+	}
 
-    for (auto [species, edge_pos] : edge) {
-        sort(edge_pos.begin(), edge_pos.end());
-        cout << species << "-" << edge_pos << endl;
+	// Map a cow's species to a prefix array that sums the contributions limited by
+	// d_desirable
+	unordered_map<int, vector<long long>> prefix_arrays;
 
-        v<ll> new_prefix = {0};
-        for (ll pos : edge_pos) {
-            new_prefix.pb(new_prefix.back() + pos);
-        }
+	// iterating by reference is important for sorting
+	for (auto &[species, edge_array] : edge_arrays) {
+		// Sort edge arrays as we iterate so we can binary search over it later
+		sort(edge_array.begin(), edge_array.end());
 
-        prefix.insert(MP(species, new_prefix));
-    }
+		vector<long long> new_prefix_array = {0};
+		for (int distance : edge_array) {
+			new_prefix_array.push_back(new_prefix_array.back() + distance);
+		}
 
-    // Iterate by cow
-    ll ans = 0;
+		prefix_arrays.insert(make_pair(species, new_prefix_array));
+	}
 
-    For (i, num_cows) { // i denotes position, position[i] denotes species
+	long long total_contributions = 0;
 
-        cout << "Current cow at "  << i << " with species " << position[i] << endl;
+	for (long long i = 0; i < num_cows; i++) {
+		int species = initial_order[i];
 
-        if (position[i] == ideal[i]) {
-            ans += ((i + 1) * i)/2;
-            ans += (num_cows - i - 1) * (num_cows - i) / 2;
-        }
+		// Catches edge case where a cow species exists in the initial ordering but not
+		// in the ideal ordering
+		if (edge_arrays.count(species) == 0) { continue; }
 
+		// Scenario 1 computation
+		long long min_distance_to_edge = min(i + 1, num_cows - i);
+		vector<int> &edge_array =
+		    edge_arrays.at(species);  // reference is important to save time
+		int index =
+		    upper_bound(edge_array.begin(), edge_array.end(), min_distance_to_edge) -
+		    edge_array.begin();  // Returns breakoff point where d_cow < d_desirable
 
-        ll curr_dist = min(i + 1, num_cows - i);
+		total_contributions += prefix_arrays.at(
+		    species)[index];  // Adds contributions limited by d_desirable
+		total_contributions +=
+		    (min_distance_to_edge) * ((long long)edge_array.size() -
+		                              index);  // Adds contributions limited by d_cow
 
-        cout << "Current distance to edge: " << curr_dist; 
+		// Scenario 2 computation
+		if (initial_order[i] == ideal_order[i]) {
+			total_contributions +=
+			    ((i * (i + 1)) / 2) + ((num_cows - i - 1) * (num_cows - i) / 2);
+		}
+	}
 
-        ll index = upper_bound(edge.at(position[i]).begin(), edge.at(position[i]).end(), curr_dist) - edge.at(position[i]).begin();
-
-        // cout << edge[position[i]] << endl;
-        cout << index << endl;
-
-        ans += prefix.at(position[i])[index];
-        ans += curr_dist * (edge.at(position[i]).size() - index);
-
-        cout << "Total countributions: " << ans << endl;
-    }
-
-    cout << ans << endl;
+	cout << total_contributions << endl;
 }
