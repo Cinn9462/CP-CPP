dbae82ba02533743afe7049da7ad03f58c25c02f
David Xiao <146673439+Cinn9462@users.noreply.github.com>
2026-06-27 21:38:08 -0700

    actually good c++ing

--- AtCoder/DP-A.cpp
+++ AtCoder/DP-A.cpp
@@ -1,43 +1,12 @@
 #define ll long long
 #define pb push_back 
 #define v vector
-#define p pair
-#define fileread(file) freopen(((string) file + ".in").c_str(), "r", stdin); freopen(((string) file + ".out").c_str(), "w", stdout)
-#define INF 1000000000000000000
-#define inf 1000000001
-#define MP(x, y) make_pair(x, y)
-#define F first
-#define S second
 #define For(i, end) for (ll i = 0; i < end; i++)
-#define f0r(i, begin, end) for (ll i = begin; i < end; i++) 
 #define nl "\n"
 #define ss " "
-#define un_map unordered_map
-#define un_set unordered_set
 
 #include <bits/stdc++.h>
 using namespace std;
-
-ostream& operator<<(ostream& os, pair<ll, ll>& p) {
-    return os << "[" << p.F << " " << p.S << "]";
-}
-ostream& operator<<(ostream& os, v<ll>& arr) {
-    for (ll ___a : arr) {
-        os << ___a << " ";
-    }
-    return os;
-}
-ostream& operator<<(ostream& os, deque<ll>& arr) {
-    for (ll ___a : arr) {
-        os << ___a << " ";
-    }
-    return os;
-}
-struct PairHash {
-    size_t operator()(const pair<ll,ll>& p) const {
-        return hash<ll>()(p.F) ^ (hash<ll>()(p.S) << 1);
-    }
-};
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
@@ -59,6 +28,5 @@ int main() {
             dp[i + 2] = min(dp[i + 2], dp[i] + abs(cost[i] - cost[i + 2]));
         }
     }
-
     cout << dp[N - 1];
 }
